39b917c0221e09fca2dc2b31d91c6e61f6f55529
“Mehmet <70447221+harunapak@users.noreply.github.com>
2022-12-16 22:57:30 +0100

    day

---
+++ IdeaProjects/Tean108JavaPractice/src/test/day5/Q3_KelimeyiHarflerineAyirma.java
@@ -0,0 +1,34 @@
+package test.day5;
+
+import java.util.Arrays;
+
+public class Q3_KelimeyiHarflerineAyirma {
+    public static void main(String[] args) {
+         /*
+         * create a custom return type method accepts a name as parameter
+         * and prints the name as a char array
+         *
+         * (do not use toCharArray() method)
+         *
+         * Input : John
+         * Output : [J, o, h, n]
+         *
+         Parametre olarak String bir isim girdiginiz, size o ismin harflerini
+         bir char Array olarak yazdiran bir method olusturunuz.*/
+
+        String name = "Zafer";
+
+       kelimeninHarfleriniAyirma(name);
+
+   }
+
+   private static void kelimeninHarfleriniAyirma(String isim) {
+
+       char[] harfler = new char[isim.length()];
+
+        for (int i = 0; i < isim.length(); i++) {
+            harfler[i] = isim.charAt(i);
+        }
+        System.out.println("Kelimenin harflerinin array'i : " + Arrays.toString(harfler));
+    }
+}
